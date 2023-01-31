5ff64fba4c03b366dd233f706de2f5c15751ef8b
Artem <mrdmitrievv@yandex.ru>
2023-01-31 17:38:30 +0300

    feat: create tableView

--- Banker/AppDelegate.swift
+++ Banker/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let onboardingContainerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
     let mainViewController = MainViewController()
+    let accountSummaryViewController = AccountSummaryViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
             
@@ -28,7 +29,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.logOutDelegate = self
         
-        window?.rootViewController = mainViewController        
+//        window?.rootViewController = mainViewController
+        window?.rootViewController = accountSummaryViewController
         
         return true
     }
